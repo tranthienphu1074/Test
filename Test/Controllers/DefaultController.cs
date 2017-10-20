6b078b7b1aac381faec5a9f243d1952d5db3d416
Phú Trần <tranthienphu1074@outlook.com>
2017-10-20 08:26:57 +0700

    Update tìm sản phẩm theo danh mục

--- Test/Controllers/DefaultController.cs
+++ Test/Controllers/DefaultController.cs
@@ -25,7 +25,79 @@ namespace Test.Controllers
                  Chuoi += "</div>";
             }
             ViewBag.View = Chuoi;
-            return View();
+            return View(product);
+        }
+
+        public ActionResult ACER()
+        {
+            string Chuoi = "";
+            var product = (from p in db.DanhMucs where p.Hang == "ACER"  select p).Take(100).ToList();
+            for (int i = 0; i < product.Count; i++)
+            {
+                Chuoi += "<div class=\"item-pro-home\">";
+                Chuoi += "<div class=\"box-img-pro-home\">";
+                Chuoi += "<img src = \"" + product[i].Image + "\" />";
+                Chuoi += "</div>";
+                Chuoi += "<a href=\"#\">" + product[i].TenSanPham + "</a>";
+                Chuoi += "<p>Giá: " + product[i].GiaTien + "</p>";
+                Chuoi += "</div>";
+            }
+            ViewBag.ACER = Chuoi;
+            return View(product);
+        }
+
+        public ActionResult ASUS()
+        {
+            string Chuoi = "";
+            var product = (from p in db.DanhMucs where p.Hang == "ASUS" select p).Take(100).ToList();
+            for (int i = 0; i < product.Count; i++)
+            {
+                Chuoi += "<div class=\"item-pro-home\">";
+                Chuoi += "<div class=\"box-img-pro-home\">";
+                Chuoi += "<img src = \"" + product[i].Image + "\" />";
+                Chuoi += "</div>";
+                Chuoi += "<a href=\"#\">" + product[i].TenSanPham + "</a>";
+                Chuoi += "<p>Giá: " + product[i].GiaTien + "</p>";
+                Chuoi += "</div>";
+            }
+            ViewBag.ASUS = Chuoi;
+            return View(product);
+        }
+
+        public ActionResult DELL()
+        {
+            string Chuoi = "";
+            var product = (from p in db.DanhMucs where p.Hang == "DELL" select p).Take(100).ToList();
+            for (int i = 0; i < product.Count; i++)
+            {
+                Chuoi += "<div class=\"item-pro-home\">";
+                Chuoi += "<div class=\"box-img-pro-home\">";
+                Chuoi += "<img src = \"" + product[i].Image + "\" />";
+                Chuoi += "</div>";
+                Chuoi += "<a href=\"#\">" + product[i].TenSanPham + "</a>";
+                Chuoi += "<p>Giá: " + product[i].GiaTien + "</p>";
+                Chuoi += "</div>";
+            }
+            ViewBag.DELL = Chuoi;
+            return View(product);
+        }
+
+        public ActionResult MAC()
+        {
+            string Chuoi = "";
+            var product = (from p in db.DanhMucs where p.Hang == "MAC" select p).Take(100).ToList();
+            for (int i = 0; i < product.Count; i++)
+            {
+                Chuoi += "<div class=\"item-pro-home\">";
+                Chuoi += "<div class=\"box-img-pro-home\">";
+                Chuoi += "<img src = \"" + product[i].Image + "\" />";
+                Chuoi += "</div>";
+                Chuoi += "<a href=\"#\">" + product[i].TenSanPham + "</a>";
+                Chuoi += "<p>Giá: " + product[i].GiaTien + "</p>";
+                Chuoi += "</div>";
+            }
+            ViewBag.MAC = Chuoi;
+            return View(product);
         }
     }
 }
